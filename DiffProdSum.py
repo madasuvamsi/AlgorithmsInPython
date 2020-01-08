eb2813ee7964240556abbb550ea60e956e521547
madasuvamsi <madasuvamsi@gmail.com>
2020-01-07 21:38:14 -0800

    Added Time and Space Compexity analysis

--- DiffProdSum.py
+++ DiffProdSum.py
@@ -2,6 +2,8 @@ n = 234
 productofNumbers = 1
 sumOfNumbers = 0
 diff = 0
+
+#Time Complexity O(n) and Space Complexity is O(1)
 for item in str(n):
     productofNumbers = productofNumbers * int(item)
     sumOfNumbers = sumOfNumbers + int(item)
